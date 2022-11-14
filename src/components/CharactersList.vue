50658ecde319385f57fc8e5ff93dd55b73e4c535
sgraziola <simo.fdl@gmail.com>
2022-11-14 15:54:18 +0100

    BreakingBad-Vite: Add CharactersList

---
+++ src/components/CharactersList.vue
@@ -0,0 +1,29 @@
+<script>
+import { store } from '../store.js'
+import CharacterItem from './CharacterItem.vue'
+
+export default {
+    name: 'CharactersList',
+    components: {
+        CharacterItem
+    },
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <section class="characters">
+        <div class="container">
+            <div class="total_found">
+                <p>Found 62 characters</p>
+            </div>
+            <div class="row row-cols-5">
+                <CharacterItem :character="character" v-for="character in store.characters" />
+            </div>
+        </div>
+    </section>
+</template>
