156c442f1de92e63f6abc83b6e37d7394497438e
sgraziola <simo.fdl@gmail.com>
2022-11-15 15:57:52 +0100

    BreakingBad-Vite: create loader comp get error

---
+++ src/components/LoadingItem.vue
@@ -0,0 +1,31 @@
+<script>
+import { store } from '../store';
+export default {
+    name: 'LoadingItem',
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="loader">
+        <img src="../" alt="">
+        <p>Attendere! Caricamento Personaggi...</p>
+    </div>
+</template>
+
+<style lang="scss">
+.loader {
+    color: black;
+    height: 100vh;
+    text-align: center;
+    margin-top: 15rem;
+
+    img {
+        width: 150px;
+    }
+}
+</style>
